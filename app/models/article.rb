971a3f227833335751e4b931082ff5647a2ba2f2
edgarjin88 <edgar.jin88@gmail.com>
2019-04-23 21:56:21 +1000

    association '

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,18 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  user_id     :integer
+#
+
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, :presence => true, length: {minimum: 3, maximum: 50}
   validates :description, :presence => true, length: {minimum: 10, maximum: 300}
-
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  username   :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User< ApplicationRecord
+
+  has_many :articles
+  before_save { self.email = email.downcase}
+  # self.email의 기본 벨류를 다시 다운케이스 하는 거다. 왜냐면 저장 전에 이미 self.eamil에 벨류가
+  # 어사인 되어 있으니까. 
   validates :username, presence: true, uniqueness: {case_sensitive: false}, 
   length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
